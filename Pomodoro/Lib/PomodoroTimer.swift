1766f2974140a8d3e58dcab19602143583db2001
grahamhealy <grahamh220@gmail.com>
2019-05-19 14:09:35 +0100

    Adding dynamic logic to enable and disable the start/pause buttons.

--- Pomodoro/Lib/PomodoroTimer.swift
+++ Pomodoro/Lib/PomodoroTimer.swift
@@ -10,8 +10,11 @@ import Foundation
 
 
 protocol PomodoroTimerDelegate {
-    func timerDidTick(seconds: Int)
+    func timerDidStart()
+    func timerDidPause()
     func timerDidEnd()
+    func timerDidTick(seconds: Int)
+    func timerDidReset(seconds: Int)
 }
 
 
@@ -21,24 +24,38 @@ class PomodoroTimer {
     var timer: Timer!
     // TODO: Set this to default value externally
     var seconds = 10
+    var paused = true
     
     init() {}
     
     func start() {
         // TODO: Do some validation to ensure the seconds variable is set
         // Start the timer and add it to the run loop for the current thread (so it runs async and wont block the UI).
+        paused = false
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerTick), userInfo: nil, repeats: true)
         RunLoop.current.add(timer, forMode: RunLoop.Mode.common)
+        if let delegate = delegate{
+            delegate.timerDidStart()
+        }
     }
     
     func pause() {
         // TODO: Pause the timer
+        paused = true
+        timer.invalidate()
+        if let delegate = delegate{
+            delegate.timerDidPause()
+        }
     }
     
     func reset() {
+        paused = true
         // TODO Reset the timer to default state
         timer.invalidate()
         seconds = 10
+        if let delegate = delegate{
+            delegate.timerDidReset(seconds: seconds)
+        }
     }
     
     @objc func timerTick() {
